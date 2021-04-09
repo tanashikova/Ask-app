2feff3ad6c13f5b06679f36230561090711f8fe2
didin21 <tanashikovadina@gmail.com>
2021-04-09 15:49:40 -0700

    debug value

--- curiosity_project/settings.py
+++ curiosity_project/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECRET_KEY = '&8mn*@j(!*^t2ynf*41=0^@jbi1+h-%8+%fr^g^s^7e4%6$3p5'
 SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
 
 ALLOWED_HOSTS = ['djangoaskapp.herokuapp.com']
 
