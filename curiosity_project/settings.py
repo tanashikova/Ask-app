276ceffe92e550920269350b4c8312d2ddea76c2
didin21 <tanashikovadina@gmail.com>
2021-04-10 21:58:16 -0700

    deletes secret key in settings

--- curiosity_project/settings.py
+++ curiosity_project/settings.py
@@ -22,7 +22,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/p
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = '&8mn*@j(!*^t2ynf*41=0^@jbi1+h-%8+%fr^g^s^7e4%6$3p5'
 SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
